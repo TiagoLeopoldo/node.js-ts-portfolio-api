110aed7c7987a26401e3bc4a802d80ed0a4b187a
TiagoLeopoldo <tnleopoldo.dev@gmail.com>
2026-01-28 02:08:23 -0300

    fix: configurar CORS e tratamento de requisições OPTIONS no Vercel

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,15 @@ export const app = async (
   request: http.IncomingMessage,
   response: http.ServerResponse,
 ) => {
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+  response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (request.method === "OPTIONS") {
+    response.writeHead(200);
+    return response.end();
+  }
+
   if (request.method === HttpMethod.GET) {
     await categoriesController(request, response);
   } else {
